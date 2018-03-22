f6820f9e904cc3a195b6d084e8a8eb4030c59152
Christopher Shaw <chris@chris-shaw.com>
2018-03-22 19:57:37 +0000

    Add Raspberry Pi Logic

---
+++ src/class/RPI.php
@@ -0,0 +1,64 @@
+<?php
+namespace App;
+
+class RPI {
+
+	public $externalIpUrl = "http://ipecho.net/plain";
+
+	public function __construct() {
+
+	}
+
+
+	public function stats () {
+
+
+		$stats = [];
+
+		$stats['ip_local'] = gethostbyname(trim(`hostname`));
+		$stats['ip_remote'] = file_get_contents($this->externalIpUrl);
+
+		// System Load
+		$stats['load'] = sys_getloadavg()[0];
+	
+		// Cores
+		$cmd = "uname";
+		$stats['os'] = strtolower(trim(shell_exec($cmd)));
+		$cpuCoreNo = null;
+		switch($stats['os']) {
+		    case('linux'):
+				$cmd = "cat /proc/cpuinfo | grep processor | wc -l";
+			break;
+		    case('freebsd'):
+		    	$cmd = "sysctl -a | grep 'hw.ncpu' | cut -d ':' -f2";
+		    break;
+		    default:
+		        unset($cmd);
+		}
+		 
+		if ($cmd != '') {
+	       $cpuCoreNo = intval(trim(shell_exec($cmd)));
+	       $stats['cpu_cores'] = $cpuCoreNo;
+		}
+
+		// Memory Usage
+		$free = shell_exec('free');
+		$free = (string)trim($free);
+		$free_arr = explode("\n", $free);
+		$mem = explode(" ", $free_arr[1]);
+		$mem = array_filter($mem);
+		$mem = array_merge($mem);
+		$stats['memory_usage'] = round($mem[2] / $mem[1] * 100).'%';
+
+		// Disk Usage
+		$disktotal = disk_total_space ('/');
+		$diskfree  = disk_free_space  ('/');
+		$stats['disk_usage'] = round (100 - (($diskfree / $disktotal) * 100)) .'%';
+	
+		if ($stats['os'] == "linux") {
+			$stats['uptime'] = floor(preg_replace ('/\.[0-9]+/', '', file_get_contents('/proc/uptime')) / 86400);
+	    }
+		return $stats;
+	}
+
+}
--- src/page/index.php
+++ src/page/index.php
@@ -1,16 +1,13 @@
 <?php
 namespace App\Page;
 use App\SpeedTest;
+use App\RPI;
 
 class Index extends \Gt\Page\Logic {
 
-	public $externalIpUrl = "http://ipecho.net/plain";
-
 	public function go() {
-
 		$this->outputSpeedTest();
 		$this->outputRpiStats();
-
 	}
 
 	public function outputSpeedTest () {
@@ -27,13 +24,35 @@ class Index extends \Gt\Page\Logic {
 		$this->document->querySelector('.php-speed-up')->textContent = $upSpeed;
 		$this->document->querySelector('.php-speed-date')->textContent = $date;
 		$this->document->querySelector('.php-speed-time')->textContent = $time;
+
+		$files = $speedtest->listLogs();
+		$date = current(array_keys($files));
+		$logs = $speedtest->analyseLog($date);
+
+		foreach ($logs as $l) {
+			$t = $this->template->get('log-row');
+			$t->querySelector('.php-date')->html = $l[0];
+			$t->querySelector('.php-time')->textContent = $l[1];
+			$t->querySelector('.php-down')->textContent = $l[2];
+			$t->querySelector('.php-up')->textContent = $l[3];
+			$t->querySelector('.php-ping')->textContent = $l[4];
+			$t->insertTemplate();
+		}
+
 	}
 
 	public function outputRpiStats() {
-		$localIP = gethostbyname(trim(`hostname`));
-		$externalIP = file_get_contents($this->externalIpUrl);
-		$this->document->querySelector('.php-ip-local')->textContent = $localIP;
-		$this->document->querySelector('.php-ip-remote')->textContent = $externalIP;
+		
+		$rpi = new RPI();
+		$stats = $rpi->stats();
+
+		$this->document->querySelector('.php-ip-local')->textContent = $stats['ip_local'];
+		$this->document->querySelector('.php-ip-remote')->textContent = $stats['ip_remote'];
+		$this->document->querySelector('.php-cpu-cores')->textContent = $stats['cpu_cores'];
+		$this->document->querySelector('.php-cpu-load')->textContent = $stats['load'];
+		$this->document->querySelector('.php-memory-usage')->textContent = $stats['memory_usage'];
+		$this->document->querySelector('.php-disk-usage')->textContent = $stats['disk_usage'];
+
 	}
 
 }
