c72d23e529903ce1825fd5437f8c27c5a546ff9c
Christopher Shaw <chris@chris-shaw.com>
2018-04-29 13:45:24 +0100

    Issue #8 - List Logs and show headings

--- src/page/tools/speed.php
+++ src/page/tools/speed.php
@@ -11,32 +11,43 @@ class Speed extends \Gt\Page\Logic {
 
 	public function outputSpeedTest () {
 
+		$this->date = $_GET['date'] ?? false;
+
 		$speedtest = new SpeedTest();
-		$latest = $speedtest->getLatest();
 
-		$downSpeed = round($latest[2],2) ?? 0.00;
-		$upSpeed = round($latest[3],2) ?? 0.00;
-		$date = $latest[0] ?? 'n/a';
-		$time = $latest[1] ?? 'n/a';
+		if ($this->date) {
+			$logs = $speedtest->analyseLog($this->date);
 
-		$this->document->querySelector('.php-speed-down')->textContent = $downSpeed;
-		$this->document->querySelector('.php-speed-up')->textContent = $upSpeed;
-		$this->document->querySelector('.php-speed-date')->textContent = $date;
-		$this->document->querySelector('.php-speed-time')->textContent = $time;
+			$t = $this->template->get('log-row');
+			$t->querySelector('.php-date')->textContent = "Date";
+			$t->querySelector('.php-time')->textContent = "Time";
+			$t->querySelector('.php-down')->textContent = "Down";
+			$t->querySelector('.php-up')->textContent = "Up";
+			$t->querySelector('.php-ping')->textContent = "Ping";
+			$t->insertTemplate();
+			
+			foreach ($logs as $l) {
+				$t = $this->template->get('log-row');
+				$t->querySelector('.php-date')->html = $l[0];
+				$t->querySelector('.php-time')->textContent = $l[1];
+				$t->querySelector('.php-down')->textContent = $l[2];
+				$t->querySelector('.php-up')->textContent = $l[3];
+				$t->querySelector('.php-ping')->textContent = $l[4];
+				$t->insertTemplate();
+			}
+			return;
+		}
 
 		$files = $speedtest->listLogs();
-		$date = current(array_keys($files));
-		$logs = $speedtest->analyseLog($date);
-
-		foreach ($logs as $l) {
-			$t = $this->template->get('log-row');
-			$t->querySelector('.php-date')->html = $l[0];
-			$t->querySelector('.php-time')->textContent = $l[1];
-			$t->querySelector('.php-down')->textContent = $l[2];
-			$t->querySelector('.php-up')->textContent = $l[3];
-			$t->querySelector('.php-ping')->textContent = $l[4];
+		
+		foreach ($files as $date => $name) {
+			$t = $this->template->get('log-list');
+			$t->querySelector('a')->textContent = $date;
+			$t->querySelector('a')->setAttribute('href',"/tools/speed?date={$date}");
 			$t->insertTemplate();
 		}
+	
+		
 
 	}
 
